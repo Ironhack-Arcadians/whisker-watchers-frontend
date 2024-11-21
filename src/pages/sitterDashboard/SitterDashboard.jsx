d7295f7b8f70ccd3af24727a70d51dda5c54e5cd
Justin Fanton <justin.fanton@gmail.com>
2024-11-21 11:12:49 +0100

    Created dashboards for pet parents and pet sitters

---
+++ src/pages/sitterDashboard/SitterDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SitterDashboard() {
+    return(
+        <div>
+            <h1>Welcome fellow pet parent!</h1>
+            <p>Lorum ipsum lorum ipsum</p>
+        </div>
+    )
+}
+
+export default SitterDashboard;
