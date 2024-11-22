d40518b5a4f65462a8157c9158bd708427c45818
Vicente Duch <vhduch@gmail.com>
2024-11-22 15:22:43 +0100

    add css to dashboard/login and navbar

--- src/pages/ownerDashboard/OwnerDashboard.jsx
+++ src/pages/ownerDashboard/OwnerDashboard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import AddPet from '../../components/AddPet';
 
 function OwnerDashboard() {
-    return(
-        <div>
+    return (
+        <div className="dashboard">
             <h1>Welcome fellow pet parent!</h1>
             <p>Lorum ipsum lorum ipsum</p>
             <AddPet />
         </div>
-    )
+    );
 }
 
 export default OwnerDashboard;
