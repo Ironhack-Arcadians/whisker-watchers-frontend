81d727857b691c9c2da236b8e719f1423354ab86
Justin Fanton <justin.fanton@gmail.com>
2024-11-22 17:01:53 +0100

    Logic to hide login and signup buttons when user is logged in

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,19 +2,25 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const isLoggedIn = localStorage.getItem("authToken");
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-link">
           <button className="navbar-button">Home</button>
         </Link>
-        <Link to="/signup" className="navbar-link">
-          <button className="navbar-button">Sign Up</button>
-        </Link>
-        <Link to="/login" className="navbar-link">
-          <button className="navbar-button">Login</button>
-        </Link>
       </div>
+      {!isLoggedIn && (
+        <div>
+          <Link to="/signup" className="navbar-link">
+            <button className="navbar-button">Sign Up</button>
+          </Link>
+          <Link to="/login" className="navbar-link">
+            <button className="navbar-button">Login</button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
