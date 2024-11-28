5e2d5c7467bde439c7d898b0383124c339ee982c
Vicente Duch <vhduch@gmail.com>
2024-11-28 15:48:35 +0100

    back button position fixed at owner dashboard

--- src/pages/OwnerCareRequestsPage/OwnerRequestsPage.jsx
+++ src/pages/OwnerCareRequestsPage/OwnerRequestsPage.jsx
@@ -37,7 +37,7 @@ const OwnerRequestsPage = () => {
 
   return (
     <div className="my-care-requests-dashboard">
-      <button className="care-back-button" onClick={() => navigate(-1)}>
+      <button className="back-button" onClick={() => navigate(-1)}>
         &larr; Back
       </button>
       <div className="my-care-requests-page">
